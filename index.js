c27cae6349248f04b9b3f373da3f20c5bd17f5de
Joseph  Benbella <benbellajoe@outlook.com>
2019-03-06 06:32:27 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,34 @@
-var kittens = [] //define your array here
+var kittens = ["Milo","Otis","Garfield"] //define your array here
 
 // Add your functions and code here
+function destructivelyAppendKitten(name){
+  kittens.push('Ralph');
+  return name
+}
+
+function destructivelyRemoveLastKitten(name){
+  kittens.pop("Garfield");
+  return name
+}
+
+function destructivelyPrependKitten(name){
+  kittens.unshift('Bob');
+  return name
+}
+
+function destructivelyRemoveFirstKitten(name){
+  kittens.shift('Milo');
+  return name
+}
+
+function appendKitten(name){
+  var newKittens = kittens.slice();
+  newKittens.push(name);
+  return newKittens
+}
+
+function prependKitten(name){
+  var newKittens = kittens.slice();
+  newKittens.pop(name);
+  return newKittens
+}
